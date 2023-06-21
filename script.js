ebbe993ee2a59c8abc62fa31c83d4c073b6abfdb
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-06-21 10:53:25 +0200

    Create function that generates password

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ const characters = {
     numbers: '0123456789',
     symbols: '!$%&|[]{}:;.,*+-#@<>~'
 }
+
+const generatePassword = () => {
+    let staticPassword = '';
+    randomPassword = '';
+    excludeDuplicate = false;
+    passLength = lengthSlider.value;
+
+    options.forEach(option => {
+        if (option.checked) {
+            if (option.id !== 'exc-duplicate' && option.is !== 'spaces') {
+                statisPassword += characters[option.id];
+            } else if (option.is === 'spaces') {
+                statisPassword += `  ${staticPassword}  `;
+            } else {
+                excludeDuplicate = true;
+            }
+        }
+    });
+
+    for (let i = 0; i < passLength; i++) {
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if (excludeDuplicate) {
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
+    }
+    passwordInput.value = randomPassword
+}
